0f7055da55e31b7620d50efc9f15f79284fdbe3f
caydenhuffman <chuffman7766@gmail.com>
2023-05-05 18:17:12 -0500

    Good Music

--- sketch.js
+++ sketch.js
@@ -1,17 +1,36 @@
 
 let playing = false;
-let myNote = ['E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D4', 'B3', 'G3', 'B3', 'D4', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', 'E4'];
+// let myNote = ['E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D4', 'B3', 'G3', 'B3', 'D4', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', 'E4'];
 // myNote = [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ,];
 
-let myDurs = ["8n", "8n", "8n", "8n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "2n", "8n", "8n", "8n", "8n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "2n"];
+// let myDurs = ["8n", "8n", "8n", "8n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "2n", "8n", "8n", "8n", "8n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "4n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "8n", "2n"];
 
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,];
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,];
 
 let quarterNotes = [null, null, null, null, null, null, 'B3', null, null, 'Bb3', null, null, 'A3', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 'B3', null, null, 'Bb3', null, null, 'A3', null, null, null, null, null, null, null, null, null, null, null, null, null,];
 let eightNotes = ['E3', 'F#3', 'G3', 'A3', 'B3', 'G3', null, 'Bb3', 'Gb3', null, 'A3', 'F3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D4', 'B3', 'G3', 'B3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', null, 'Bb3', 'Gb3', null, 'A3', 'F3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', null,];
 // let doubleNotes = [['E3', null], ['F#3', null], ['G3', null], ['A3', null], ['B3', null], ['G3', null], [null, null] ['Bb3', null], ['Gb3', null], [null, null] ['A3', null], ['F3', null], [null, null] ['E3', null], ['F#3', null], ['G3', null], ['A3', null], ['B3', null], ['G3', null], ['B3', null], ['E4', null], ['D4', null], ['B3', null], ['G3', null], ['B3', null], [null, null] ['E3', null], ['F#3', null], ['G3', null], ['A3', null], ['B3', null], ['G3', null], [null, null] ['Bb3', null], ['Gb3', null], [null, null] ['A3', null], ['F3', null], [null, null] ['E3', null], ['F#3', null], ['G3', null], ['A3', null], ['B3', null], ['G3', null], ['B3', null], ['E4', null], ['D#4', null], ['B3', null], ['D#4', null], ['F#4', null], [null, null] ]; 
 
 let doubleNotes = [[null, null, null, null, null, null, 'B3', null, null, 'Bb3', null, null, 'A3', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 'B3', null, null, 'Bb3', null, null, 'A3', null, null, null, null, null, null, null, null, null, null, null, null, null,], ['E3', 'F#3', 'G3', 'A3', 'B3', 'G3', null, 'Bb3', 'Gb3', null, 'A3', 'F3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D4', 'B3', 'G3', 'B3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', null, 'Bb3', 'Gb3', null, 'A3', 'F3', null, 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', null,]];
+
+// let myNote = ["E4", "D#4", "E4", "C#5", "A3", "E4", "D#4", "E4", "C#5", "A3"];
+// let myNote = ["E4", null, null "E4", null, null, "E4", null, null, "E4"];
+
+// let myNote = [["E4"], ["D#4", "E4", "C#4"], ["A3", "E4"], ["D#4", "E4", "C#4"], "A3"]; 
+
+// let myNote = [['E3', 'F#3', 'G3'], ['A3', 'B3', 'G3'], 'B3', ['Bb3', 'Gb3'], 'Bb3', ['A3', 'F3'], 'A3', ['E3', 'F#3', 'G3'], ['A3', 'B3', 'G3'], 'B3', ['E4', 'D4', 'B3'], ['G3', 'B3'], 'D4', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', 'E4'];
+
+// let myNote = [['E3', 'E3', 'E3', 'E3'], "F#3", 'G3', ['A3', 'B3', 'G3'], 'B3', ['Bb3', 'Gb3'], 'Bb3', ['A3', 'F3'], 'A3', ['E3', 'F#3', 'G3'], ['A3', 'B3', 'G3'], 'B3', ['E4', 'D4', 'B3'], ['G3', 'B3'], 'D4', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'Bb3', 'Gb3', 'Bb3', 'A3', 'F3', 'A3', 'E3', 'F#3', 'G3', 'A3', 'B3', 'G3', 'B3', 'E4', 'D#4', 'B3', 'D#4', 'F#4', 'E4'];
+// let myNote = [
+//     ['E3', null, 'F#3', null], ['G3', null, 'A3', null], ['B3', null, 'G3', null], ['B3', null], ['Bb3', null, 'Gb3', null], ["Bb3", null], ['A3', null, 'F3', null], ['A3', null],
+//     ['E3', null, 'F#3', null], ['G3', null, 'A3', null], ['B3', null, 'G3', null], ['B3', null, 'E4', null], ['D4', null, 'B3', null], ['G3', null, 'B3', null], 'D4',
+//     ['E3', null, 'F#3', null], ['G3', null, 'A3', null], ['B3', null, 'G3', null], ['B3', null], ['Bb3', null, 'Gb3', null], ["Bb3", null], ['A3', null, 'F3', null], ['A3', null],
+//     ['E3', null, 'F#3', null], ['G3', null, 'A3', null], ['B3', null, 'G3', null], ['B3', null, 'E4', null],
+//     ['D#4', null, 'B3', null], ['D#4', null, 'F#4', null], 'E4'];
+
+myNote = [
+    ['E3', null, null, null], [null, null, null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null],
+    [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], null, [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null], [null, null, null, null], [null, null, null]];
 
 let instrument = new Tone.FMSynth().toDestination();
 
@@ -37,15 +56,14 @@ duoSynth.set({ "vibratoAmount": 0.5, "vibratoRate": 5, "harmonicity": 1.5, "voic
 let smallerFreverb = new Tone.Freeverb();
 smallerFreverb.set({ "roomSize": 0.7, "dampening": 4300, "wet": 0.4 });
 // duoSynth.connect(smallerFreverb);
-// smallerFreverb.connect(Tone.Master); 
-duoSynth.toDestination();//Delete if using previous. 
+// smallerFreverb.connect(Tone.Master);
+duoSynth.toDestination();//Delete if using previous.
 
-
-myhalfNotes = [, , , ,];
 
 let synth = new Tone.Synth().toDestination();
 
 let pattern;
+
 
 function setup() {
     canv = createCanvas(800, 600).parent("sketchHolder");
@@ -54,21 +72,18 @@ function setup() {
     //     instrument.triggerAttackRelease(note[1], "8n", time);;
     //     // duoSynth.triggerAttackRelease(note, "4n", time);
     // }, doubleNotes);
-
+    console.log(myNote);
     pattern = new Tone.Sequence((time, note) => {
-        duoSynth.triggerAttackRelease(myNote[note], myDurs[note], time);
-        instrument.triggerAttackRelease(quarterNotes[note], '2n', time);
+        instrument.triggerAttackRelease(note, "2n", time);
+        console.log("Played " + note);
+    }, myNote);
 
-        // console.log("8: " + eightNotes[note] + " 4: " + quarterNotes[note]);
-        console.log("Played " + myNote[note] + " for " + myDurs[note]); 
-        // subdivisions are given as subarrays
-    }, numbers);
 
     // pattern2 = new Tone.Pattern(function (time, note){
     //     duoSynth.triggerAttackRelease(note, "2n", time); 
     // }, myhalfNotes);
 
-    // Tone.Transport.bpm.value = 200;
+    Tone.Transport.bpm.value = 50;
 
 }
 
@@ -98,45 +113,48 @@ function keyPressed() {
     switch (keyCode) {
         case 49:
             console.log(1);
-            addNotes(['E3', [0, 13, 26, 39]]);
+            addNotes(['E3', [[0, 0], [8, 0], [15, 0], [23, 0]]]);
             break;
         case 50:
             console.log(2);
-            addNotes(['B3', [4, 6, 17, 19, 22, 24, 30, 32, 43, 45, 48]]);
+            addNotes(['B3', [[2, 0], [3, 0], [10, 0], [11, 0], [12, 2], [13, 2], [17, 0], [18, 0], [25, 0], [26, 0], [27, 2]]]);
             break;
         case 51:
             console.log(3);
-            addNotes(['Gb3', [8, 34]]);
-            addNotes(['F#4', [50]])
+            addNotes(['Gb3', [[4, 2], [19, 2]]]);
+            addNotes(['F#4', [[28, 2]]])
             break;
         case 52:
             console.log(4);
-            addNotes(['E4', [20, 46, 51]]);
+            myNote[29] = 'E4';
+            addNotes(['E4', [[11, 2], [26, 2]]]);
+
             break;
         case 53:
             console.log(5);
-            addNotes(['D#4', [47, 49]]);
-            addNotes(['D4', [21, 25]]);
+            myNote[14] = 'D4';  
+            addNotes(['D#4', [[27, 0], [28, 0]]]);
+            addNotes(['D4', [[12, 0]]]);
             break;
         case 54:
             console.log(6);
-            addNotes(['G3', [2, 5, 15, 18, 23, 28, 31, 41, 44]]);
+            addNotes(['G3', [[1, 0], [2, 2], [9, 0], [10, 2], [13, 0], [16, 0], [17, 2], [24, 0], [25, 2]]]);
             break;
         case 55:
             console.log(7);
-            addNotes(['F3', [11, 37]]);
+            addNotes(['F3', [[6, 2], [21, 2]]]);
             break;
         case 56:
             console.log(8);
-            addNotes(['Bb3', [7, 9, 33, 35]]);
+            addNotes(['Bb3', [[4, 0], [5, 0], [19, 0], [20, 0]]]);
             break;
         case 57:
             console.log(9);
-            addNotes(['A3', [3, 10, 12, 16, 29, 36, 38, 42]]);
+            addNotes(['A3', [[1, 2], [6, 0], [7, 0], [9, 2], [16, 2], [21, 0], [22, 0], [24, 2]]]);
             break;
         case 48:
             console.log(0);
-            addNotes(['F#3', [1, 14, 27, 40]]);
+            addNotes(['F#3', [[0, 2], [8, 2], [15, 2], [23, 2]]]);
             break;
 
         case RIGHT_ARROW:
@@ -147,12 +165,30 @@ function keyPressed() {
     // console.log(keyCode);
 }
 
+// function addNotes(array) {
+//     pattern.stop();
+//     for (i = 0; i < array[1].length; i++) {
+//         myNote[array[1][i]] = array[0];
+//     }
+//     pattern.start();
+// }
 function addNotes(array) {
     pattern.stop();
-    for (i = 0; i < array[1].length; i++) {
-        myNote[array[1][i]] = array[0];
+    for (let i = 0; i < array[1].length; i++) {
+        // for (let j = 0; j < array[1][i].length; j++) {
+        // myNote[array[1][i][j]] = array[0];
+        myNote[array[1][i][0]][array[1][i][1]] = array[0];
+        // }
     }
+
+    pattern = new Tone.Sequence((time, note) => {
+        instrument.triggerAttackRelease(note, "2n", time);
+        console.log("Played " + note);
+    }, myNote);
+
     pattern.start();
+    console.log(myNote);
+
 }
 
 
@@ -160,7 +196,17 @@ function addNotes(array) {
 
 
 
-
-
-
-
+[
+    ['E3', [[0, 0], [8, 0], [15, 0], [23, 0]]],
+    ['F#3', [[0, 2], [8, 2], [15, 2], [23, 2]]],
+    ['G3', [[1, 0], [2, 2], [9, 0], [10, 2], [13, 0], [16, 0], [17, 2], [24, 0], [25, 2]]],
+    ['A3', [[1, 2], [6, 0], [7, 0], [9, 2], [16, 2], [21, 0], [22, 0], [24, 2]]],
+    ['F3', [[6, 2], [21, 2]]],
+    ['Bb3', [[4, 0], [5, 0], [19, 0], [20, 0]]],
+    ['B3', [[2, 0], [3, 0], [10, 0], [11, 0], [12, 2], [13, 2], [17, 0], [18, 0], [25, 0], [26, 0], [27, 2]]],
+    ['D4', [[12, 0]]],
+    ['D#4', [[27, 0], [28, 0]]],
+    ['Gb3', [[4, 2], [19, 2]]],
+    ['F#4', [[28, 2]]],
+    ['E4', [[11, 2], [26, 2]]], //And then [29]
+]
